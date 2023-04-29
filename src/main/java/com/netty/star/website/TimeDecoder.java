8bcdd4f7c4824dd6e6cee3137d5b73166fbc3fa6
star362 <star362@163.com>
2023-04-29 13:08:29 +0800

    demo

---
+++ src/main/java/com/netty/star/website/TimeDecoder.java
@@ -0,0 +1,23 @@
+package com.netty.star.website;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+
+import java.util.List;
+
+/**
+ * @Author star362
+ * @Date 2023/4/28 21:30
+ * @Description: TODO
+ */
+public class TimeDecoder extends ByteToMessageDecoder { // (1)
+    @Override
+    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) { // (2)
+        if (in.readableBytes() < 4) {
+            return; // (3)
+        }
+
+        out.add(in.readBytes(4)); // (4)
+    }
+}
